b2b5da611bea2df59a0dd3f7dbbaf3a00acbe444
Yuji Takaesu <taka.esu.user@gmail.com>
2020-10-14 17:28:56 +0900

    feat: display body content from markdown using remark

--- next-ts/official-tutorial/nextjs-blog/pages/posts/[id].js
+++ next-ts/official-tutorial/nextjs-blog/pages/posts/[id].js
@@ -1,14 +1,21 @@
+import Head from "next/head";
 import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -24,7 +31,9 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // params.idを使用して、ブログの投稿に必要なデータを取得する
-  const postData = getPostData(params.id);
+  // remark使っているのでawaitする
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
